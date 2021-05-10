66b766a306c2b0660d9c7272e5e86815ff9a97ca
mukulsharma05175 <73372165+mukulsharma05175@users.noreply.github.com>
2021-05-10 10:19:12 +0530

    Delete hal.sh

--- hal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-alias gcl='git clone'
-
-cd ../../hardware/qcom-caf/msm8996/ && rm -rf audio media display && gcl https://github.com/SuperCosmicBeing/media -b caf-msm8996-r media && gcl https://github.com/SuperCosmicBeing/display -b caf-msm8996-r display && gcl https://github.com/SuperCosmicBeing/audio -b caf-msm8996-r audio && cd -
